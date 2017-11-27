016f93f94069db63f16d91691c4867cc3d9e3346
Febon <32780971+Febon@users.noreply.github.com>
2017-11-27 23:23:34 +0200

    eclipse project
    κατεβαστε το

---
+++ Ergasia/src/main/java/dictionary/Dictionary_Creator.java
@@ -0,0 +1,108 @@
+package dictionary;
+
+import java.util.Scanner;
+
+import Comparison.RunComparison;
+import suggestion.Suggestion;
+import textSplit.TextSplit;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+
+public class Dictionary_Creator {
+	public static final Map<String, String> dic1 = new HashMap<String, String>();
+	public static final Map<String, Float> dic2 = new HashMap<String, Float>();
+	public static Map<String, Integer[]>  numberOfWords = new HashMap<String, Integer[]>();
+
+	public static String[] latinLetters = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+	public static String[] greekLetters = {"á", "Ü", "â", "ã", "ä", "å", "Ý", "æ", "ç", "Þ", "è", "é", "ß", "ê", "ë", "ì", "í", "î", "ï", "ü", "ð", "ñ", "ó", "ô", "õ", "ý", "ö", "÷", "ø", "ù", "þ"};
+	static String english = "C:\\Users\\anton\\ant\\Ergasia\\dictionaries\\english_lowercase.txt";
+	static String greek = "C:\\Users\\anton\\ant\\Ergasia\\dictionaries\\greek_lowercase.txt";
+	public static void  fillMap(String fileName) {
+		try {
+			if (fileName.equals(english)) {
+				
+				numberOfWords = fillNumberOfWords(english);
+			}else {
+				
+				numberOfWords = fillNumberOfWords(greek);
+			}
+			File f1 = new File(fileName);
+			FileReader fr = new FileReader(f1);
+			BufferedReader br = new BufferedReader(fr);
+			String line = null;
+			String previousLetter = "";
+			int i = 0;
+			int letter = 0;
+			int previous = 0;
+			while ((line = br.readLine()) != null) {
+				dic1.put(line.substring(0,1) + i,line);
+				dic2.put(line.substring(0,1) + i, (float)0);
+				if(!(line.substring(0,1).equals(previousLetter)) && (i!=0)) {
+					numberOfWords.put(previousLetter ,new Integer[] {previous, i});
+					letter++;
+					previous = i;
+				}
+				previousLetter = line.substring(0,1);
+				i++;
+			}
+			fr.close();
+			br.close();
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+    }
+	public static void createDictionary(String languageCode) {
+		if (languageCode.equals("English")) {
+			fillMap(english);
+		}else {
+			fillMap(greek);
+		}
+	}
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("give language");
+		String language = sc.nextLine();
+		sc.close();
+		createDictionary(language);
+	}
+	public static boolean opperateDictionary(String  givenWord) {
+		Boolean answer = false; 
+		if(!(TextSplit.symbols.contains(givenWord.charAt(0)))) {
+			answer = RunComparison.totalComparison(givenWord);
+			if (!answer) {
+				float percentage;
+				String letter = givenWord.substring(0,1);
+				for (int i = numberOfWords.get(letter)[0] ; i < numberOfWords.get(letter)[1];i++) {
+					percentage = RunComparison.partialComparison (givenWord, dic1.get(letter + i));
+					dic2.put(letter + i, percentage);
+				}
+				String[] suggest = Suggestion.findSuggestions(dic2, letter, numberOfWords.get(letter)[0] , numberOfWords.get(letter)[1]);
+				TextSplit.correctedText.get(givenWord).setSuggestion1(suggest[0]);
+				TextSplit.correctedText.get(givenWord).setSuggestion2(suggest[1]);;
+				TextSplit.correctedText.get(givenWord).setSuggestion3(suggest[2]);;
+	
+			}
+		}
+		return answer;
+	}
+	public static Map<String,Integer[]> fillNumberOfWords( String languageCode) {
+		Map<String, Integer[]>  numberOfWordsTemporary = new HashMap<String, Integer[]>();
+		if(languageCode.equals(english)) {
+			for(int i = 0; i < latinLetters.length;i++) {
+				numberOfWordsTemporary.put(latinLetters[i], new Integer[] {0,0});
+			}
+		} else if(languageCode.equals(greek)) {
+			for(int i = 0; i < greekLetters.length;i++) {
+				numberOfWordsTemporary.put(greekLetters[i], new Integer[] {0,0});
+			}
+		}
+		
+		return numberOfWordsTemporary;
+	}
+}
---
+++ Ergasia/src/main/java/textSplit/TextSplit.java
@@ -0,0 +1,148 @@
+package textSplit;
+
+/**
+ *TextSplit
+ *
+ *Copyright 2017 Karagianni/team java autocorrect
+ */
+
+import java.util.Map;
+import java.util.List;
+import java.util.Arrays;
+import java.util.HashMap;
+import suggestion.Suggestion;
+
+/**
+ *class TextSplit reads text from user and splits it into words
+ *@version 1.0 29 Oct 2017
+ *@author Kostantina Karagianni
+ */
+
+public class TextSplit {
+	public static Map<Integer,String> textForCorrection = new HashMap<Integer,String>();
+	public static Map<String,Suggestion> correctedText= new HashMap<String,Suggestion>();
+	public static Character[] symb = {',','.','<','>','?',':',';','\'','"','(',')','{','}','[',']','/','\\','!','@','%','$','#','&','*','~','-','_','`','+','=','0','1','2','3','4','5','6','7','8','9'};
+	public static List<Character> symbols = Arrays.asList(symb);
+	protected static void splitIntoWords(String txt){
+
+		String[] a = txt.split(" ");
+
+		for (int i=0; i<a.length; i++) {
+
+			textForCorrection.put(i,a[i]);
+			correctedText.put(a[i], new Suggestion(a[i], null, null, null));
+		}
+		Arrays.sort(symb);
+		for (int i = 0; i <textForCorrection.size(); i++) {
+			for(int z = 0; z < symb.length; z++) {
+				if (textForCorrection.get(i).contains( ".*" + symb[z] + ".*" )) {
+					String[] splt = correctedText.get(textForCorrection.get(i)).getWord().split(String.valueOf(symb[z]));
+					String currentWord = textForCorrection.get(i);
+					if (!(currentWord.substring(0,1).equals(String.valueOf(symb[z])) || currentWord.substring(currentWord.length()-1,currentWord.length()).equals(String.valueOf(symb[z])))){
+						
+						Extra(i, splt,symb[z], "middle");
+						
+					} else if (currentWord.substring(0,1).equals(String.valueOf(symb[z])) && !(currentWord.substring(1,currentWord.length()).contains(".*" + String.valueOf(symb[z]) + ".*"))) {
+						
+						Extra(i, splt,symb[z], "left");
+						
+					} else if(currentWord.substring(currentWord.length()-1,currentWord.length()).equals(String.valueOf(symb[z])) && !(currentWord.substring(0,currentWord.length()-1).contains(".*" + String.valueOf(symb[z]) + ".*"))) {
+						
+						Extra(i, splt,symb[z], "right");
+						
+					} else if (currentWord.substring(0,1).equals(String.valueOf(symb[z])) && currentWord.substring(1, currentWord.length()).contains( ".*" + String.valueOf(symb[z]) + ".*")) {
+						
+						Extra(i, splt,symb[z], "midle-left");
+						
+					} else if(currentWord.substring(currentWord.length()-1,currentWord.length()).equals(String.valueOf(symb[z])) && currentWord.substring(0,currentWord.length()-1).contains(".*" + String.valueOf(symb[z]) + ".*") ) {
+						
+						Extra(i, splt,symb[z], "midle-right");
+						
+					} else if(currentWord.substring(0,1).equals(String.valueOf(symb[z])) && currentWord.substring(1,currentWord.length()-1).equals(String.valueOf(symb[z]))) {
+						
+						Extra(i, splt,symb[z], "left-right");
+						
+					} else if(currentWord.substring(0,1).equals(String.valueOf(symb[z])) && currentWord.substring(currentWord.length()-1,currentWord.length()).equals(String.valueOf(symb[z]))) {
+						
+						Extra(i, splt,symb[z], "left-right");
+						
+					} else {
+						
+						Extra(i, splt,symb[z], "everywhere");
+					}
+					break; 
+				}
+			}
+		}
+	}
+	protected static void Extra(int pointer,String[] array, char containedCharacter, String location) {
+			int extraSpace = 1;
+			List<String> list = Arrays.asList(array);
+		if (location.equals("middle")) {
+			
+			extraSpace = 2*(array.length) - 1;
+			list = addCharacter(extraSpace, list, containedCharacter);
+			
+		} else if (location.equals("left")) {
+			
+			list = addCharacterLeftOrRight(extraSpace, list, containedCharacter, "left");
+			
+		} else if(location.equals("right")) {
+			
+			list = addCharacterLeftOrRight(extraSpace, list, containedCharacter, "right");
+			
+		} else if(location.equals("middle-left")) {
+			
+			extraSpace = 2*(array.length);
+			list = addCharacter(extraSpace, list, containedCharacter);
+			list = addCharacterLeftOrRight(extraSpace, list, containedCharacter, "left");
+			
+		} else if(location.equals("middle-right")) {
+			
+			extraSpace = 2*(array.length);
+			list = addCharacter(extraSpace, list, containedCharacter);
+			list = addCharacterLeftOrRight(extraSpace, list, containedCharacter, "right");
+
+		} else if (location.equals("left-right")) {
+			extraSpace = 2;
+			list = addCharacterLeftOrRight(extraSpace, list, containedCharacter, "left");
+			list = addCharacterLeftOrRight(extraSpace, list, containedCharacter, "right");
+		} else {
+			list = addCharacter(extraSpace, list, containedCharacter);
+			list = addCharacterLeftOrRight(extraSpace, list, containedCharacter, "left");
+			list = addCharacterLeftOrRight(extraSpace, list, containedCharacter, "right");
+		}
+		for(int i = correctedText.size()+array.length + extraSpace - 1; i>pointer+1; i--) {
+			textForCorrection.put(i,textForCorrection.get(i-array.length));
+			correctedText.put(textForCorrection.get(i), correctedText.get(textForCorrection.get(i-array.length)));
+		}
+		for(int i = pointer;i < array.length + extraSpace; i++) {
+			textForCorrection.put(i,array[i]);
+			correctedText.put(array[i], new Suggestion(array[i], null, null, null));
+		}
+	}
+	public static List<String> addCharacter(int extraSpace, List<String> list, char containedCharacter) {
+		for (int i = 0; i < extraSpace; i++) {
+			list.set(i*2,list.get(i));
+			if(i!=0) {
+				list.set(i * 2 - 1,String.valueOf(containedCharacter));
+			}
+			
+		}
+		return list;
+	}
+	public static List<String> addCharacterLeftOrRight(int extraSpace, List<String> list, char containedCharacter, String position) {
+		if(position.equals("left")){
+			for(int i = list.size(); i>1; i--) {
+				list.set(i,list.get(i-1));
+			}
+			list.set(0,String.valueOf(containedCharacter));
+		} else {
+			list.add(String.valueOf(containedCharacter));
+		}
+		return list;		
+	}
+
+		
+	
+}
